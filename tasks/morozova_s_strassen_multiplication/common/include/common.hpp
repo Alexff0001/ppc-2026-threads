10d9e7619dfcb4f2f4cd49c816413380f692d824
SonyaMorozova <sonyasmorozova@gmail.com>
2026-03-04 01:14:41 +0300

    fix: clang-tidy

--- tasks/morozova_s_strassen_multiplication/common/include/common.hpp
+++ tasks/morozova_s_strassen_multiplication/common/include/common.hpp
@@ -17,7 +17,7 @@ struct Matrix {
   std::vector<double> data;
   int size;
 
-  Matrix(int n) : size(n), data(n * n, 0.0) {}
+  Matrix(int n) : data(n * n, 0.0), size(n) {}
 
   double &operator()(int i, int j) {
     return data[i * size + j];

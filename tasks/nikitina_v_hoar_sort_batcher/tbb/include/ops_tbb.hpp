8b07e18bd2a07408ea32856371c534f22c243158
Лера Никитина <valerianikitina56436@gmail.com>
2026-04-07 11:21:09 +0000

    fix tidy

--- tasks/nikitina_v_hoar_sort_batcher/tbb/include/ops_tbb.hpp
+++ tasks/nikitina_v_hoar_sort_batcher/tbb/include/ops_tbb.hpp
@@ -2,7 +2,6 @@
 
 #include "nikitina_v_hoar_sort_batcher/common/include/common.hpp"
 #include "task/include/task.hpp"
-
 namespace nikitina_v_hoar_sort_batcher {
 
 class HoareSortBatcherTBB : public BaseTask {

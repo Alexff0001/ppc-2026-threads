dd4574fbbf74ab35d820e96e766033455b9b7031
vlad3005 <vladborunov396@gmail.com>
2026-04-13 08:10:13 +0300

    pull before request

--- tasks/borunov_v_complex_ccs/tests/performance/main.cpp
+++ tasks/borunov_v_complex_ccs/tests/performance/main.cpp
@@ -61,8 +61,8 @@ TEST_P(BorunovVRunPerfTestThreads, RunPerfModes) {
 
 namespace {
 
-const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, BorunovVComplexCcsOMP, BorunovVComplexCcsSEQ>(PPC_SETTINGS_borunov_v_complex_ccs);
+const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType, BorunovVComplexCcsOMP, BorunovVComplexCcsSEQ>(
+    PPC_SETTINGS_borunov_v_complex_ccs);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
